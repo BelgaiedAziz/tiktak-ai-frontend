8f617fb3949149650c1bbe798b6603e76cb2cf2b
Bassem Kallel <kallelbassem@bizerte.r-iset.tn>
2026-03-07 14:07:39 +0100

    chore: remove unused files (apis, hooks, contexts, components)

--- src/constants/intents.js
+++
@@ -1,9 +0,0 @@
-export const INTENTS = {
-  PRIX: 'prix',
-  COMMANDE: 'commande',
-  SUIVI: 'suivi',
-  RECLAMATION: 'reclamation',
-  SALUTATION: 'salutation'
-};
-
-export const CONFIDENCE_THRESHOLD = 0.60;
